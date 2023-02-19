5a2f6629efdfebdf7255d2f88f12228f33a0586e
Moses <mosesflava@gmail.com>
2023-02-19 14:18:41 +0000

    First Commit

---
+++ Source/FlotillaCommander/FlotillaCommander.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FlotillaCommander.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlotillaCommander, "FlotillaCommander" );
---
+++ Source/FlotillaCommander/FlotillaCommanderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlotillaCommanderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOTILLACOMMANDER_API AFlotillaCommanderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
