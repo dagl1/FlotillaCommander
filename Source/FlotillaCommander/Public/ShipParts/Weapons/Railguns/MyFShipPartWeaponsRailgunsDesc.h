846400f38b868e5217a845c69dc6a74e3c6a0e87
dagl1 <jellebonthuis@hotmail.com>
2023-02-21 17:33:53 +0100

    AddedExtraClassesAndComponents

---
+++ Source/FlotillaCommander/Public/ShipParts/Weapons/Railguns/MyFShipPartWeaponsRailgunsDesc.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ShipParts/Weapons/FShipPartWeaponsBaseDesc.h"
+#include "MyFShipPartWeaponsRailgunsDesc.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLOTILLACOMMANDER_API UMyFShipPartWeaponsRailgunsDesc : public UFShipPartWeaponsBaseDesc
+{
+	GENERATED_BODY()
+	
+};
